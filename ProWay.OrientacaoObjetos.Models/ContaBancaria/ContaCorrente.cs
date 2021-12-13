78290e13d82e685c891a6599297ae3c759402c35
isaacarellan <isaacrivas20122000@gmail.com>
2021-12-13 19:02:10 -0300

    Criado a classe de conta corrente

---
+++ ProWay.OrientacaoObjetos.Models/ContaBancaria/ContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProWay.OrientacaoObjetos.Models.ContaBancaria
+{
+    internal class ContaCorrente
+    {
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+        public string Codigo { get; set; }
+    }
+}
